f1948b1f247a7ad044f088730feb622f3317ab37
Sylvain Robez-Masson <srobez-masson@commentcamarche.net>
2014-08-10 21:41:09 +0200

    include fixture Bouh

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/fixtures/model/Bouh.php';
 require __DIR__ . '/fixtures/model/BouhRepository.php';
 require __DIR__ . '/fixtures/FakeDriver/Driver.php';
 require __DIR__ . '/fixtures/FakeDriver/MysqliResult.php';
