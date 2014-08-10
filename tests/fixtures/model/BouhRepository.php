1fc6537978d902a3005c245f3b1ceb4af14a3fc3
Sylvain Robez-Masson <srobez-masson@commentcamarche.net>
2014-08-10 19:54:29 +0200

    Fix fixture BouhRepository with the new initMetadata

--- tests/fixtures/model/BouhRepository.php
+++ tests/fixtures/model/BouhRepository.php
@@ -2,11 +2,22 @@
 
 namespace tests\fixtures\model;
 
+use fastorm\Entity\Metadata;
+use fastorm\Entity\MetadataRepository;
+
 class BouhRepository extends \fastorm\Entity\Repository
 {
-    public static function loadMetadata(\fastorm\Entity\Metadata $metadata)
+    public static function initMetadata(MetadataRepository $metadataRepository = null, Metadata $metadata = null)
     {
+        if ($metadataRepository === null) {
+            $metadataRepository = MetadataRepository::getInstance();
+        }
 
+        if ($metadata === null) {
+            $metadata = new Metadata();
+        }
+
+        $metadata->setClass(get_class());
         $metadata->setConnection('main');
         $metadata->setDatabase('bouh_world');
         $metadata->setTable('T_BOUH_BOO');
@@ -17,6 +28,6 @@ class BouhRepository extends \fastorm\Entity\Repository
            'columnName' => 'boo_id'
         ));
 
-        return $metadata;
+        $metadata->addInto($metadataRepository);
     }
 }
