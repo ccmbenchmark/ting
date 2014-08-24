94f8b9df3ab6be5a1fb5bc20d07eebe6146b826a
Sylvain Robez-Masson <srobez-masson@commentcamarche.net>
2014-08-24 17:38:49 +0200

    Add Result Pgsql

---
+++ lib/fastorm/Driver/Pgsql/Result.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace fastorm\Driver\Pgsql;
+
+use fastorm\Driver\QueryException;
+
+class Result implements \fastorm\Driver\ResultInterface
+{
+
+    protected $result          = null;
+    protected $fields          = array();
+    protected $iteratorOffset  = 0;
+    protected $iteratorCurrent = null;
+
+    public function __construct($result)
+    {
+        $this->result = $result;
+    }
+
+    public function setQuery($query)
+    {
+        $aliasToTable = array();
+        $fields = array();
+
+        preg_match_all(
+            '/(?:join|from)\s+"?(?<table>[a-z0-9_]+)"?\s*(?:as)?\s*"?(?!on)(?<alias>[a-z0-9_]+)?"?(\s|$)/is',
+            $query,
+            $matches,
+            PREG_SET_ORDER
+        );
+        foreach ($matches as $match) {
+            $match['table'] = strtolower($match['table']);
+            if ($match['alias'] !== '') {
+                $aliasToTable[strtolower($match['alias'])] = $match['table'];
+            } else {
+                $aliasToTable[$match['table']] = $match['table'];
+            }
+        }
+
+        $tableToAlias = array_flip($aliasToTable);
+        preg_match_all('/select\s+(.+?)(\s+from|$)/is', $query, $matches, PREG_SET_ORDER);
+
+        if (count($matches) === 0) {
+            throw new queryException('Query invalid: can\'t parse columns');
+        }
+
+        $queryColumns = $matches[0][1];
+
+        if (strpos($queryColumns, '*') !== false) {
+            throw new QueryException('Query invalid: usage of asterisk in column definition is forbidden');
+        }
+
+        preg_match_all(
+            '/(?:(?:"?(?<table>[a-z0-9_]+)"?\.)?(?<column>[a-z0-9_]+)(?:\s*(?:as)?\s+(?<alias>[a-z0-9_]+))?)/is',
+            $queryColumns,
+            $matches,
+            PREG_SET_ORDER
+        );
+        foreach ($matches as $match) {
+            $stdClass = new \stdClass();
+            $stdClass->orgname = $match['column'];
+
+            if (isset($match['alias']) === true) {
+                $stdClass->name = $match['alias'];
+            } else {
+                $stdClass->name = $stdClass->orgname;
+            }
+
+            if ($match['table'] !== '') {
+                $stdClass->table    = strtolower($match['table']);
+                $stdClass->orgtable = $aliasToTable[$stdClass->table];
+            } else {
+                $stdClass->orgtable = strtolower(pg_field_table($this->result, count($fields)));
+                $stdClass->table    = $tableToAlias[$stdClass->orgtable];
+            }
+
+            $fields[] = $stdClass;
+        }
+
+        $this->fields = $fields;
+    }
+
+    public function dataSeek($offset)
+    {
+        return pg_result_seek($this->result, $offset);
+    }
+
+    public function format($data)
+    {
+        if ($data === false) {
+            return null;
+        }
+
+        $columns = array();
+        $data = array_values($data);
+
+        foreach ($this->fields as $i => $rawField) {
+            $column = array(
+                'name'     => $rawField->name,
+                'orgName'  => $rawField->orgname,
+                'table'    => $rawField->table,
+                'orgTable' => $rawField->orgtable,
+                'value'    => $data[$i]
+            );
+
+            $columns[] = $column;
+        }
+
+        return $columns;
+    }
+
+    /**
+     * Iterator
+     */
+    public function rewind()
+    {
+        $this->dataSeek(0);
+        $this->iteratorOffset = -1;
+        $this->next();
+    }
+
+    public function current()
+    {
+        return $this->iteratorCurrent;
+    }
+
+    public function key()
+    {
+        return $this->iteratorOffset;
+    }
+
+    public function next()
+    {
+        $this->iteratorCurrent = $this->format(pg_fetch_array($this->result, null, PGSQL_NUM));
+        $this->iteratorOffset++;
+    }
+
+    public function valid()
+    {
+        if ($this->iteratorCurrent !== null) {
+            return true;
+        }
+
+        return false;
+    }
+}
