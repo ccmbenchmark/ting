00f19547d0c650b3ee1d7a428524b72035612341
Xavier Leune <xleune@commentcamarche.net>
2014-08-26 14:12:22 +0200

    Query: setParams & setDriver

--- lib/fastorm/Query.php
+++ lib/fastorm/Query.php
@@ -11,6 +11,7 @@ class Query
 
     protected $sql = '';
     protected $params = array();
+    protected $driver = null;
 
     public function __construct($sql, $params = array())
     {
@@ -18,11 +19,47 @@ class Query
         $this->params = $params;
     }
 
+    /**
+     * @param array $params
+     * @return $this
+     * @throws \InvalidArgumentException
+     */
+    public function setParams($params)
+    {
+        if (!is_array($params)) {
+            throw new \InvalidArgumentException('Params should be an array');
+        }
+        $this->params = $params;
+
+        return $this;
+    }
+
+    /**
+     * @param DriverInterface $driver
+     * @return $this
+     */
+    public function setDriver(DriverInterface $driver)
+    {
+        $this->driver = $driver;
+
+        return $this;
+    }
+
+    /**
+     * @param Collection $collection
+     * @return mixed
+     * @throws QueryException
+     */
     public function execute(
-        DriverInterface $driver,
         Collection $collection = null
     ) {
-        $driver->prepare(
+        if ($this->driver === null) {
+            throw new QueryException('You have to set the driver before to call execute');
+        }
+
+        return $this->driver->executeSimpleQuery($this->sql, $this->params, $collection);
+
+        /*$this->driver->prepare(
             $this->sql,
             function (
                 StatementInterface $statement,
@@ -36,6 +73,6 @@ class Query
             }
         );
 
-        return $result;
+        return $result;*/
     }
 }
