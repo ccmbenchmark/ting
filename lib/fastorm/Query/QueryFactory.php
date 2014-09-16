3be13a8bd37ed17377a32618daf91305a5f4e5a5
Sylvain Robez-Masson <srobez-masson@commentcamarche.net>
2014-09-16 23:09:17 +0200

    QueryFactory for better dependancy injection

---
+++ lib/fastorm/Query/QueryFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace fastorm\Query;
+
+class QueryFactory implements QueryFactoryInterface
+{
+    public function get($params = [])
+    {
+        return new Query($params);
+    }
+
+    public function getPrepared($params = [])
+    {
+        return new PreparedQuery($params);
+    }
+}
