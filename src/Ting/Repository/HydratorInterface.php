4789159f1c7290711dc4833eb2c0e6b2ca1d4787
Sylvain Robez-Masson <srobez-masson@commentcamarche.net>
2014-10-15 16:26:34 +0200

    Add interface and CollectionFactory

---
+++ src/Ting/Repository/HydratorInterface.php
@@ -0,0 +1,34 @@
+<?php
+/***********************************************************************
+ *
+ * Ting - PHP Datamapper
+ * ==========================================
+ *
+ * Copyright (C) 2014 CCM Benchmark Group. (http://www.ccmbenchmark.com)
+ *
+ ***********************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ **********************************************************************/
+
+namespace CCMBenchmark\Ting\Repository;
+
+use CCMBenchmark\Ting\MetadataRepository;
+use CCMBenchmark\Ting\UnitOfWork;
+
+interface HydratorInterface
+{
+    public function __construct(MetadataRepository $metadaRepository, UnitOfWork $unitOfWork);
+    public function hydrate(array $columns = array(), Collection $collection);
+}
