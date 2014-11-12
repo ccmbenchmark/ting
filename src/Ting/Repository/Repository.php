cbb6c1b2491fc69853edb3403f2d695bef0d0467
Xavier Leune <xleune@commentcamarche.net>
2014-11-12 17:00:10 +0100

    CodingStyle

--- src/Ting/Repository/Repository.php
+++ src/Ting/Repository/Repository.php
@@ -149,7 +149,7 @@ class Repository
             $this->queryFactory,
             $this->collectionFactory,
             $primariesKeyValue,
-            (bool) $forceMaster
+            (bool)$forceMaster
         );
 
         if ($forceMaster) {
