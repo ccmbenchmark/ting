09d5ac079206e5de5a72847f478cfdd9e7d903a3
srm <srm@bouh.org>
2015-02-18 17:09:30 +0100

    Add documentations about hydrator

--- src/Ting/Repository/Hydrator.php
+++ src/Ting/Repository/Hydrator.php
@@ -67,6 +67,12 @@ class Hydrator implements HydratorInterface
 
     /**
      * Hydrate one object from values
+     *
+     * @internal hydrate all column into the right Entity according to the table name and metadata information
+     *           all virtual columns (COUNT(*), etc) will be set in the array key 0
+     *           all Entities without any information (a "LEFT JOIN user" can return no informatoin at all about user)
+     *              are set to null
+     *
      * @param array               $columns
      * @return array
      */
@@ -74,10 +80,12 @@ class Hydrator implements HydratorInterface
     {
         $result        = [];
         $metadataList  = [];
-        $tmpEntities   = [];
-        $validEntities = [];
+        $tmpEntities   = []; // Temporary entity when all properties are null for the moment (LEFT/RIGHT JOIN)
+        $validEntities = []; // Entity marked as valid will fill an object
+                             // (a valid Entity is a entity with at less one property not null)
 
         foreach ($columns as $column) {
+            // We have the information table, it's not a virtual column like COUNT(*)
             if (isset($result[$column['table']]) === false) {
                 $this->metadataRepository->findMetadataForTable(
                     $column['orgTable'],
@@ -89,13 +97,16 @@ class Hydrator implements HydratorInterface
                 );
             }
 
+            // We have a metadata defined for the column
             if (
                 isset($metadataList[$column['table']]) === true &&
                 $metadataList[$column['table']]->hasColumn($column['orgName']) === true
             ) {
+                // Column value is null or entity is still not marked as valid
                 if ($column['value'] === null && isset($validEntities[$column['table']]) === false) {
                     $tmpEntities[$column['table']][$column['orgName']] = $result[$column['table']];
                 } else {
+                    // Entity was previously marked as a temporary entity, we set all previous columns retrieved
                     if (isset($tmpEntities[$column['table']]) === true && $tmpEntities[$column['table']] !== []) {
                         foreach ($tmpEntities[$column['table']] as $entityColumn => $entity) {
                             $metadataList[$column['table']]->setEntityProperty(
@@ -104,6 +115,7 @@ class Hydrator implements HydratorInterface
                                 null
                             );
                         }
+                        unset($tmpEntities[$column['table']]);
                     }
 
                     $validEntities[$column['table']] = true;
@@ -114,6 +126,8 @@ class Hydrator implements HydratorInterface
                         $column['value']
                     );
                 }
+
+            // Table is not mapped or column is a virtual column
             } else {
                 $validEntities[0] = true;
                 if (isset($result[0]) === false) {
@@ -125,6 +139,7 @@ class Hydrator implements HydratorInterface
         }
 
         foreach ($result as $table => $entity) {
+            // All no valid entity is replaced by a null value
             if (isset($validEntities[$table]) === false) {
                 $result[$table] = null;
             }
