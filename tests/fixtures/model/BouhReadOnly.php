fd60234170976bd33fe08f0970b291ab9b84362b
Sylvain Robez-Masson <sylvain@ccmbenchmark.com>
2017-07-25 08:47:18 +0200

    Add missing file

---
+++ tests/fixtures/model/BouhReadOnly.php
@@ -0,0 +1,124 @@
+<?php
+/***********************************************************************
+ *
+ * Ting - PHP Datamapper
+ * ==========================================
+ *
+ * Copyright (C) 2014 CCM Benchmark Group. (http://www.ccmbenchmark.com)
+ *
+ ***********************************************************************
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ **********************************************************************/
+
+namespace tests\fixtures\model;
+
+class BouhReadOnly
+{
+    protected $id        = null;
+    protected $firstname = null;
+    protected $name      = null;
+    protected $enabled   = null;
+    protected $price     = null;
+    protected $roles     = ['USER'];
+    protected $city      = null;
+    protected $retrievedTime = null;
+    protected $originalCity = null;
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+    }
+
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
+    }
+
+    public function setEnabled($enabled)
+    {
+        $this->enabled = $enabled;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getRoles()
+    {
+        return $this->roles;
+    }
+
+    public function getEnabled()
+    {
+        return $this->enabled;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function setRetrievedTime($time)
+    {
+        $this->retrievedTime = $time;
+    }
+
+    public function getRetrievedTime()
+    {
+        return $this->retrievedTime;
+    }
+
+    public function setCity(City $city)
+    {
+        $this->originalCity = clone $city;
+        $this->city = $city;
+    }
+
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    public function getOriginalCity()
+    {
+        return $this->originalCity;
+    }
+}
