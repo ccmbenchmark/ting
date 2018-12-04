def82745ffbb86f9b3a2e8f5e50d36fe69da36cf
Matthias de MAUROY <mdemauroy@ccmbenchmark.com>
2018-12-04 15:55:05 +0100

    Feature: adding query order and limit clause to getBy() - create missing tests + cleanup

--- tests/units/Ting/Query/Generator.php
+++ tests/units/Ting/Query/Generator.php
@@ -97,6 +97,53 @@ class Generator extends atoum
         ;
     }
 
+    public function testGetByCriteriaWithArrayValueAndOrderShouldReturnAQuery()
+    {
+        $services = new \CCMBenchmark\Ting\Services();
+
+        $this
+            ->if(
+                $generator = new \CCMBenchmark\Ting\Query\Generator(
+                    $this->mockConnection,
+                    $this->mockQueryFactory,
+                    '',
+                    'table',
+                    ['id', 'population']
+                )
+            )
+            ->object($generator->getByCriteria(['name' => ['Xavier','Olivier']], ['name' => 'ASC'], null,$services->get('CollectionFactory'), true))
+                ->isInstanceOf('\CCMBenchmark\Ting\Query\Query')
+            ->mock($this->mockConnection)
+                ->call('master')
+                    ->once()
+
+        ;
+    }
+
+
+    public function testGetByCriteriaWithArrayValueAndOrderLimitShouldReturnAQuery()
+    {
+        $services = new \CCMBenchmark\Ting\Services();
+
+        $this
+            ->if(
+                $generator = new \CCMBenchmark\Ting\Query\Generator(
+                    $this->mockConnection,
+                    $this->mockQueryFactory,
+                    '',
+                    'table',
+                    ['id', 'population']
+                )
+            )
+            ->object($generator->getByCriteria(['name' => ['Xavier','Olivier']], ['name' => 'ASC'], 1,$services->get('CollectionFactory'), true))
+                ->isInstanceOf('\CCMBenchmark\Ting\Query\Query')
+            ->mock($this->mockConnection)
+                ->call('master')
+                    ->once()
+
+        ;
+    }
+
     public function testGetByCriteriaWithArrayValueShouldReturnAQuery()
     {
         $services = new \CCMBenchmark\Ting\Services();
@@ -111,7 +158,7 @@ class Generator extends atoum
                     ['id', 'population']
                 )
             )
-            ->object($generator->getByCriteria(['name' => ['Xavier','Olivier']], $services->get('CollectionFactory'), true))
+            ->object($generator->getByCriteria(['name' => ['Xavier','Olivier']], null, null,$services->get('CollectionFactory'), true))
                 ->isInstanceOf('\CCMBenchmark\Ting\Query\Query')
             ->mock($this->mockConnection)
                 ->call('master')
@@ -134,7 +181,7 @@ class Generator extends atoum
                     ['id', 'population']
                 )
             )
-            ->object($generator->getByCriteria(['name' => 'Xavier'], $services->get('CollectionFactory'), true))
+            ->object($generator->getByCriteria(['name' => 'Xavier'], null, null,$services->get('CollectionFactory'), true))
                 ->isInstanceOf('\CCMBenchmark\Ting\Query\Query')
             ->mock($this->mockConnection)
                 ->call('master')
