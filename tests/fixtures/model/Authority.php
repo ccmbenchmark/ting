7e88128057437a6453844a43f7aafe12857f221f
Sylvain Duval <sduval@ccmbenchmark.com>
2023-11-13 17:10:16 +0100

    fix(hydratorRelational): sort config from source and target dependencies

--- tests/fixtures/model/Authority.php
+++ tests/fixtures/model/Authority.php
@@ -27,17 +27,11 @@ namespace tests\fixtures\model;
 use CCMBenchmark\Ting\Entity\NotifyProperty;
 use CCMBenchmark\Ting\Entity\NotifyPropertyInterface;
 
-class City implements NotifyPropertyInterface
+class Authority implements NotifyPropertyInterface
 {
-
     use NotifyProperty;
 
-    protected $id        = null;
-    protected $name      = null;
-    protected $zipcode   = null;
-    protected $bouhId    = null;
-    protected $parks     = [];
-    protected $department = null;
+    protected $id = null;
 
     public function setId($id)
     {
@@ -45,61 +39,8 @@ class City implements NotifyPropertyInterface
         $this->id = $id;
     }
 
-    public function setName($name)
-    {
-        $this->propertyChanged('name', $this->name, $name);
-        $this->name = $name;
-    }
-
-    public function setZipcode($zipcode)
-    {
-        $this->propertyChanged('zipcode', $this->zipcode, $zipcode);
-        $this->zipcode = (string) $zipcode;
-    }
-
-    public function setBouhId($bouhId)
-    {
-        $this->propertyChanged('bouhId', $this->bouhId, $bouhId);
-        $this->bouhId = (string) $bouhId;
-    }
-
     public function getId()
     {
         return $this->id;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getZipcode()
-    {
-        return $this->zipcode;
-    }
-
-    public function getBouhId()
-    {
-        return $this->bouhId;
-    }
-
-    public function parksAre(array $parks)
-    {
-        $this->parks = $parks;
-    }
-
-    public function getParks()
-    {
-        return $this->parks;
-    }
-
-    public function setDepartment(Department $department)
-    {
-        $this->department = $department;
-    }
-
-    public function getDepartment()
-    {
-        return $this->department;
     }
 }
--- tests/fixtures/model/Department.php
+++ tests/fixtures/model/Department.php
@@ -27,17 +27,13 @@ namespace tests\fixtures\model;
 use CCMBenchmark\Ting\Entity\NotifyProperty;
 use CCMBenchmark\Ting\Entity\NotifyPropertyInterface;
 
-class City implements NotifyPropertyInterface
+class Department implements NotifyPropertyInterface
 {
 
     use NotifyProperty;
 
-    protected $id        = null;
-    protected $name      = null;
-    protected $zipcode   = null;
-    protected $bouhId    = null;
-    protected $parks     = [];
-    protected $department = null;
+    protected $id = null;
+    protected $authority = null;
 
     public function setId($id)
     {
@@ -45,61 +41,18 @@ class City implements NotifyPropertyInterface
         $this->id = $id;
     }
 
-    public function setName($name)
-    {
-        $this->propertyChanged('name', $this->name, $name);
-        $this->name = $name;
-    }
-
-    public function setZipcode($zipcode)
-    {
-        $this->propertyChanged('zipcode', $this->zipcode, $zipcode);
-        $this->zipcode = (string) $zipcode;
-    }
-
-    public function setBouhId($bouhId)
-    {
-        $this->propertyChanged('bouhId', $this->bouhId, $bouhId);
-        $this->bouhId = (string) $bouhId;
-    }
-
     public function getId()
     {
         return $this->id;
     }
 
-    public function getName()
+    public function setAuthority(Authority $authority)
     {
-        return $this->name;
+        $this->authority = $authority;
     }
 
-    public function getZipcode()
+    public function getAuthority()
     {
-        return $this->zipcode;
-    }
-
-    public function getBouhId()
-    {
-        return $this->bouhId;
-    }
-
-    public function parksAre(array $parks)
-    {
-        $this->parks = $parks;
-    }
-
-    public function getParks()
-    {
-        return $this->parks;
-    }
-
-    public function setDepartment(Department $department)
-    {
-        $this->department = $department;
-    }
-
-    public function getDepartment()
-    {
-        return $this->department;
+        return $this->authority;
     }
 }
