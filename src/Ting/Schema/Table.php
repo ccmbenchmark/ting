27cae9bcaaad9839cd7f69cdaa289ce48720e7ed
Xavier Leune <xavier.leune@gmail.com>
2024-12-16 23:52:40 +0100

    Remove comments

--- src/Ting/Schema/Table.php
+++ src/Ting/Schema/Table.php
@@ -5,7 +5,7 @@ namespace CCMBenchmark\Ting\Schema;
 #[\Attribute(\Attribute::TARGET_CLASS)]
 readonly class Table
 {
-    public function __construct(public string $name, public string $connection, public string $database, public ?string $repository = null)
+    public function __construct(public string $name, public string $connection, public string $database, public string $repository)
     {
         
     }
