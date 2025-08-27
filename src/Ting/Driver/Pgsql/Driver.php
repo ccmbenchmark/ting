e5ec5fb1d9cbb6835300263ed03a90a7d32eb11a
D'Ali Ben Macha <contact@benmacha.tn>
2025-08-27 23:54:56 +0200

    Fix #120: SQL Injection Vulnerability in PostgreSQL Driver

--- src/Ting/Driver/Pgsql/Driver.php
+++ src/Ting/Driver/Pgsql/Driver.php
@@ -440,8 +440,8 @@ class Driver implements DriverInterface
      */
     public function getInsertedIdForSequence($sequenceName)
     {
-        $sql = "SELECT currval('$sequenceName')";
-        $resultResource = @pg_query($this->connection, $sql);
+        $sql = "SELECT currval($1)";
+        $resultResource = @pg_query_params($this->connection, $sql, [$sequenceName]);
 
         if ($resultResource === false) {
             throw new QueryException(pg_last_error($this->connection) . ' (Query: ' . $sql . ')');
